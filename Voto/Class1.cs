17f4b3f56fd535a8c6decd880864f6a27b30f3ab
etec <etec@L05M06>
2025-03-24 20:34:18 -0300

    Adicionar arquivos de projeto.

---
+++ Voto/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voto
+{
+   public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public bool Pode {  get; set; }
+
+
+
+        public Pessoa(string pNome, int pIdade)
+        {
+            Nome = pNome;
+            Idade = pIdade;
+        }
+
+
+
+        public podeVotar(int Idade, bool Pode)
+        {
+            if (Idade < 16 | Idade >= 0)
+            {
+                Pode = false;
+            }
+            else if (Idade <= 17 | Idade >= 16)
+            {
+                Pode = true;
+            }
+            else
+            {
+                Pode = true;
+            }
+
+            return Pode;
+        }
+
+}
---
+++ Voto/Program.cs
@@ -0,0 +1,6 @@
+﻿using Voto;
+namespace Voto;
+public string Pessoa1 {  get; set; }
+internal class validação { 
+    Pessoa1 = pe
+}
